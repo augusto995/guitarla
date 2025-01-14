a2abfe3414d19cb8491561a002712068fb5d78b0
Augusto Ana <augustora95@gmail.com>
2025-01-14 12:20:46 -0300

    75.1 State derivado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,13 @@ export default function Header({cart}) {
       <header className="py-5 header">
         <div className="container-xl">
             <div className="row justify-content-center justify-content-md-between">
+                
                 <div className="col-8 col-md-3">
                     <a href="index.html">
                         <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
                     </a>
                 </div>
+
                 <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                     <div 
                         className="carrito"
