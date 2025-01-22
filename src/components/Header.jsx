0396e03e3468b31b29961a497eaa47f70f2747f7
Augusto Raul Ana <augustora95@gmail.com>
2025-01-22 17:48:59 -0300

    76. Calculando el total a pagar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header({cart}) {
 
     //State derivado
     const isEmpty = () => cart.length === 0
+    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
 
   return (
       <header className="py-5 header">
@@ -29,6 +30,8 @@ export default function Header({cart}) {
                                 <p className="text-center">El carrito esta vacio</p>
                             ) : (
 
+                            <>
+
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -41,9 +44,12 @@ export default function Header({cart}) {
                                 </thead>
                                 <tbody>
                                     {cart.map( guitar => ( 
+                                    //Muestra el codigo una 
+                                    // vez por cada elemento del cart
                                         <tr key={guitar.id}>
                                             <td>
-                                                <img className="img-fluid" 
+                                                <img 
+                                                className="img-fluid" 
                                                 src={`./img/${guitar.image}.jpg`}
                                                 alt="imagen guitarra" 
                                             />
@@ -79,10 +85,11 @@ export default function Header({cart}) {
                                     ))}
                                 </tbody>
                             </table>
+                            <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                            </>
+                            
+                            )}
 
-)}
-
-                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                         </div>
                     </div>
