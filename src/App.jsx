cb3eded5441ecd2cdad0e173737ea306067119f1
Augusto Raul Ana <augustora95@gmail.com>
2025-01-23 15:28:42 -0300

    81. Agregando la función de Limpiar Carrito

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,10 @@ function App() {
     setCart(updatedCart)
   }
 
+  function clearCart() {
+    setCart([])
+  }
+
 
   return (
     <>
@@ -65,6 +69,7 @@ function App() {
       removeFromCart={removeFromCart}
       decreaseQuantity={decreaseQuantity}
       increaseQuantity={increaseQuantity}
+      clearCart={clearCart}
     />
       
     <main className="container-xl mt-5">
